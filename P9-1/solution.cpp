94b6625afb7b83d9b1828d0b04622873e2228f8e
huckleberri6 <j-devore.1@onu.edu>
2023-10-26 12:07:01 -0400

    Every element in order of appearance in the array

--- P9-1/solution.cpp
+++ P9-1/solution.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+const int SIZE = 10;
+
+int main()
+{
+	srand(time(0));
+	int nums[SIZE];
+	cout << "The random integers: ";
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		int num = rand() % 10 + rand() % 100 + rand() % 1000 + rand() % 10000 - 1000;
+		nums[i] = num;
+		cout << nums[i] << " ";
+	}
+
+}
